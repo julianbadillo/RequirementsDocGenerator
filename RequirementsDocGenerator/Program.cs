aac843da486bc4e17c3fb06e7ca4acc25504ec9f
jbadillo <julianbadillo@gmail.com>
2021-11-29 11:56:55 -0600

    Using arg variables for entry point.

--- RequirementsDocGenerator/Program.cs
+++ RequirementsDocGenerator/Program.cs
@@ -6,10 +6,18 @@ namespace RequirementsDocGenerator
     {
         static void Main(string[] args)
         {
-            // TODO use command lines
+            //string projectPath = @"C:\Users\jbadillo\Documents\workspace\RequirementsDocGenerator\RequirementsDocGenerator";
+            //gen.Generate($"{projectPath}/requirements.xlsx", $"{projectPath}/requirements.docx");
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Usage: Program <INPUT FILE> <OUTPUT FILE>");
+                return;
+            }
             var gen = new DocGenerator();
-            string projectPath = @"C:\Users\jbadillo\Documents\workspace\RequirementsDocGenerator\RequirementsDocGenerator";
-            gen.Generate($"{projectPath}/requirements.xlsx", $"{projectPath}/requirements.docx");
+            Console.WriteLine($"Input: {args[0]}");
+            Console.WriteLine($"Output: {args[1]}");
+            gen.Generate(args[0], args[1]);
+            Console.WriteLine("Document generated");
         }
     }
 }
